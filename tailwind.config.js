d1c6bea8177d1f503e81d25c8e69d4fcc9600722
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-03-18 17:52:47 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+colors: {
+rawdapurple: '#6938EF' ,
+rawdayellow: '#D9D9D9'  ,
+rawdawhite:   '#FAF9F9' ,
+rawdablack: '1E1E1E' ,
+mehdi: 'CEA9EC'
+
+}
+
+
+    },
+  },
+  plugins: [],
+}
