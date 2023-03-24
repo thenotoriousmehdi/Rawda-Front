e581f3f95469ce60b337e9b5130aebcf4cc9a6aa
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-03-24 18:55:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./Components/Header";
 //import Headerr from "./Components/Headerr.js";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
-/* import About from "./Components/About"; */
+import About from "./Components/About"; 
 /* import FaqCard from "./Components/FaqCard"; */
 /* import Faq from "./Components/Faq"; */
 //import Login from "./Components/Login"
@@ -14,7 +14,7 @@ function App() {
     <div className="bg-rawdawhite">
   <Header />
   <Hero />
-{/*   <About />  */}
+  <About /> 
  {/*  <Faq/> */}
 {/*   <FaqCard/> */}
   <Footer/>
