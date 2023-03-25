d41b952d3eb9c02ed298e7a41a1c9cc13ed980a4
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-03-25 16:11:25 +0100

    home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import Header from "../Components/Header";
+import Hero from "../Components/Hero";
+import Footer from "../Components/Footer";
+import About from "../Components/About"; 
+
+ function Home(){
+return (
+    <>
+     <Header /> 
+     <Hero /> 
+     <About />
+     <Footer />
+    </>
+);
+ }
+ export default Home;
