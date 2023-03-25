08a7b1579dc078b8a120ec35eccfa7bb3d15aad6
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-03-25 15:48:45 +0100

    update about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,20 +1,20 @@
 
  const About = () => {
   return (
-    <div id="about" className="opacity-60 bg-rawdapurple  ">
+    <div id="about" className="mx-8 opacity-60 bg-rawdapurple  ">
        <div className="mx-auto  sm:px-6 lg:px-10 ">
         <div className="relative flex-col mx-2 my-2 w-full h-max justify-center items-center md:mb-10 mb-4"> 
       <div className="container flex flex-wrap p-6 md:flex-row 
         items-center justify-between px-6 mx-auto mt-10 space-y-0 md:space-y-0">
           <div className="flex flex-col gap-6">
           <h1
-            className=" text-4xl font-bold text-center text-rawdablack justify-center
+            className=" text-4xl font-bold text-center text-rawdawhite justify-center
             md:text-4xl md:center "
           >
             A propos de Rawda.
           </h1>
           <div>
-          <text className="text-white mt-20 font-light md:text-xl text-xl leading-9 text-center justify-center mb-8">
+          <text className="text-rawdawhite mt-20 font-light md:text-xl text-xl leading-9 text-center justify-center mb-8">
             Rawda est un moteur de recherche de crèches en Algérie. Nous
             comprenons à quel point le choix de la crèche est crucial pour le
             développement de votre enfant. C'est pourquoi nous avons créé Rawda.
