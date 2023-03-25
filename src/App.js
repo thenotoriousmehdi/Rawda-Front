952a0044858e9d8a1e2cc06617ed5137f6210416
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-03-25 16:18:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import Header from "./Components/Header";
-//import Headerr from "./Components/Headerr.js";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
 import CarteCreche from "./Components/CarteCreche";
  import About from "./Components/About"; 
-/* import FaqCard from "./Components/FaqCard"; */
-/* import Faq from "./Components/Faq"; */
-//import Login from "./Components/Login"
-//import Signup from "./Components/Signup"
+ import Home from "./Pages/Home";
 
 function App() {
   return (
     
     <div className="bg-rawdawhite">
-  <Header />
-  <Hero />
-<About /> 
- {/*  <Faq/> */}
-{/*   <FaqCard/> */}
-{/*  <CarteCreche/>  */}
-  <Footer/>
-      
+     <Home /> 
     </div>
   );
 }
