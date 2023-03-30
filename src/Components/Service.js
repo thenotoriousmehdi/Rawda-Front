3b86cdcee98b8d9f7f2451c877a7134058b2fd47
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-03-30 18:57:21 +0100

    Create Service.js

---
+++ src/Components/Service.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Services(props) {
+    return(
+        <div className="  p-5 w-[350px] h-[400px] border border-rawdapurple border-opacity-60 md:p-8 bg-rawdawhite  xl:w-[400px] md:w-[30%] xl:h-[500px] md:h-[500px] shadow shadow-rawdapurple rounded-[8px]">
+        <img src={props.img}></img>
+        <h3 className=" text-rawdapurple text-base  md:text-xl md:pt-8 pt-4 text-center  font-Poppins font-bold">{props.title}</h3>
+        <p className="font-Poppins text-rawdablack text-opacity-80 text-[12px] font-light md:text-lg text-center">{props.description}</p>
+        </div>
+          );
+    }
