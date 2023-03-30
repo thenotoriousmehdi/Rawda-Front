dcddc55f6e52d977c14d7d0c6ca547346711d07c
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-03-31 00:39:25 +0100

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,18 +1,27 @@
 
  const About = () => {
   return (
-    <div id="about" className="mx-8 opacity-60 bg-rawdapurple  ">
+    <div id="about" className="mx-8  bg-opacity-30 bg-rawdapurple  ">
        <div className="mx-auto  sm:px-6 lg:px-10 ">
         <div className="relative flex-col mx-2 my-2 w-full h-max justify-center items-center md:mb-10 mb-4"> 
       <div className="container flex flex-wrap p-6 md:flex-row 
         items-center justify-between px-6 mx-auto mt-10 space-y-0 md:space-y-0">
           <div className="flex flex-col gap-6">
-          <h1
-            className=" text-4xl font-bold text-center text-rawdawhite justify-center
-            md:text-4xl md:center "
+
+
+          <div className="flex flex-col items-center gap md:gap-y-0 ">
+        <p className="  text-center text-3xl md:text-4xl font-Poppins font-bold text-rawdablack z-10">  A propos de Rawda.</p>
+      <div className="  mb-[0.5px] w-[200px] md:w-[325px] h-[20px] md:mt-[-15px] bg-rawdayellow shadow-rawdayellow "></div></div>
+
+
+          {/* <h1
+            className=" text-4xl font-bold text-center text-rawdablack justify-center
+            md:text-4xl md:center z-10"
           >
             A propos de Rawda.
-          </h1>
+          </h1> */}
+
+
           <div>
           <text className="text-rawdawhite mt-20 font-light md:text-xl text-xl leading-9 text-center justify-center mb-8">
             Rawda est un moteur de recherche de crèches en Algérie. Nous
@@ -43,39 +52,3 @@ export default About;
 
 
 
-
-{/* const About = () => {
-  return (
-    <div id="about" className="opacity-30 bg-rawdapurple  ">
-       <div className="mx-auto  sm:px-6 lg:px-10 ">
-        <div className="relative flex flex-row h-8 p-6 items-center justify-between"></div>
-      
-        <img height={300} width={10} alt="deco"></img> 
-      </div>
-    
-        <div className="relative flex-wrap w-full h-max justify-center items-center md:mb-10 mb-4">
-          <h1
-            className=" text-4xl font-bold text-center text-black justify-center
-            md:text-4xl md:center"
-          >
-            A propos de Rawda.
-          </h1>
-
-          <text className="text-white mt-20 font-light md:text-xl text-xl leading-9 text-center justify-center font-sans mb-36">
-            Rawda est un moteur de recherche de crèches en Algérie. Nous
-            comprenons à quel point le choix de la crèche est crucial pour le
-            développement de votre enfant. C'est pourquoi nous avons créé Rawda.
-            Notre mission est de vous permettre de trouver une crèche de qualité
-            pour votre enfant tout en préservant vos informations personnelles.
-            De plus, Rawda est une vitrine en ligne pour les propriétaires de
-            crèches. vous pouvez pas imaginer le temps que rawda vous fait
-            gagner ! Essayez Rawda dès maintenant et profitez de toutes les
-            fonctionnalités qu'elle offre.
-          </text>
-        </div>
-    </div>
-  );
-};
-
-export default About; 
- */}
