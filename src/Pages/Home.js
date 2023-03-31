f45361d74db5b8bbc8b6f78d2b6685eaf9f36b92
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-03-31 02:32:23 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,18 +4,21 @@ import Footer from "../Components/Footer";
 import About from "../Components/About"; 
 import Promo from "../Components/Promo";
 import deco1 from "../assets/deco1.png";
-import Services from "../Components/Services";
+import Services from "../Components/service/Services.js";
+import Faq from "../Components/Faq/Faq.js";
  function Home(){
 return (
     <>
      <Header /> 
      <Hero /> 
+   
      <div>
             <img src={deco1} />
           </div>
      <About />
      {/* <Promo /> */}
      <Services />
+     <Faq />
      <Footer />
     </>
 );
