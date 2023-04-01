608625c7097177d93415ebe47c51e771403a4641
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-04-01 20:37:29 +0100

    Update CrecheCard.js

--- src/Components/CrecheCard.js
+++ src/Components/CrecheCard.js
@@ -4,16 +4,16 @@ export default function CrecheCard(){
 return(
 
 
-  <div className="w-[300px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-12">
+  <div className="w-[300px] bg-white border border-gray-200 rounded-lg shadow  m-12">
   <a href="#">
       <img className="rounded-t-lg" src={mehdi} alt="" />
   </a>
   <div className="p-5">
       <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Crèche berceau des anges</h5>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">Crèche berceau des anges</h5>
       </a>
       <p className="mb-3 font-sans text-gray-700 dark:text-gray-400"> Birkhadem, Alger</p>
-      <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-rawdapurple rounded-lg hover:bg-rawdapurple hover:bg-opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-rawdapurple rounded-lg hover:bg-rawdapurple hover:bg-opacity-80 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
           Details
           <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </a>
