153b6a96568ca233c138be6fc25de258aba1eddb
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-04-03 18:40:25 +0100

    Update AvisCard.js

--- src/Components/AvisCard.js
+++ src/Components/AvisCard.js
@@ -5,28 +5,28 @@ export default function AvisCrd ({ photo, nom, avis })
 {
 return (
 
-<div className="keen-slider__slide h-max w-max  bg-rawdapurple bg-opacity-[3%]  flex flex-col justify-center items-center p-24 rounded-lg">
- <div className=" flex flex-col jusitify-center items-center  w-[600px] h-[300px] gap-6 border border-dashed border-rawdapurple mx-4 rounded-md">
+<div className="keen-slider__slide h-max w-max  bg-rawdapurple bg-opacity-[3%]  flex justify-center  p-24 rounded-lg mb-24">
+ <div className=" flex jusitify-center w-[300px] h-[300px] md:w-[600px] md:h-[300px] gap-6 border border-dashed border-rawdapurple mx-4 rounded-md">
   
-          <div className="flex flex-col justify-center gap">
+          <div className="flex justify-center gap">
 
            {/*  <div className=" h-[150px] w-[150px] rounded-full  mt-[-51px] ml-[-51px] border-2 border-dashed border-rawdayellow"> */} 
-          <img
-                          className="h-[130px] w-[130px] rounded-full z-30 mt-[-50px] ml-[-50px] border-2 border-rawdapurple "
+           <img
+                          className="  h-[70px] w-[70px] md:h-[130px] md:w-[130px] rounded-full z-30 mt-[-80px] ml-[-200px] md:mt-[-70px] md:ml-[-100px] border-2 border-rawdapurple "
                           src={photo}
 
                           alt=""
-                        />
+                        /> 
                       {/*   </div> */}
 
-<div className="bg-transparent h-full mt-[-120px] mr-[-100px] z-[-20px] relative flex flex-col justify-center items-center md:h-96 h-110 lg:w-2/3 md:w-4/5 sm:w-5/6 w-5/6 p-5 rounded">
+<div className="flex flex-col justify-center items-center h-full mt-[-50px] mr-[-60px]  relative  md:h-96 h-110 lg:w-2/3 md:w-4/5 sm:w-5/6 w-5/6 p-5 rounded">
          
          
           <div className="text-left w-9/12 font-extrabold font-Poppins text-2xl mb-4 text-rawdapurple">
             {nom}
           </div>
   
-  <p className="text-left font-sans font-light text-rawdablack mt-5 text-xl">
+  <p className="text-left font-sans font-light text-rawdablack mt-5 text-l">
     {avis}
   </p>
 
