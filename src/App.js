71216bc6a573c6bacb02a88a40bc420f0553cd5a
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-04-26 13:12:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Header from "./Components/Header";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
-import CarteCreche from "./Components/CarteCreche";
-import About from "./Components/About";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
+import Profile from "./Pages/Profile";
+ import About from "./Components/About"; 
+ import Home from "./Pages/Home";
+ import AddCreche from "./Pages/AddCreche";
+ import Login from "./Pages/Login";
+ import Signup from "./Pages/Signup";
+ import mehdi from "./assets/mehdi.jpeg";
+import Notification from "./Components/Notification";
+import Notifications from "./Pages/Notifications";
+import ModifierProfil from "./Components/ModifierProfil";
 
 function App() {
   return (
