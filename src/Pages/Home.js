735473c7412486cd33b6b19e743a2364d8fdb4ea
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-04-26 13:14:00 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,35 +4,27 @@ import Footer from "../Components/Footer";
 import About from "../Components/About";
 import Promo from "../Components/Promo";
 import deco1 from "../assets/deco1.png";
-import Services from "../Components/Services.js";
-import Faq from "../Components/Faq.js";
-import Avis from "../Components/Avis";
-import AvisCard from "../Components/AvisCard";
-import FaqCard from "../Components/FaqCard";
-import CrecheCard from "../Components/CrecheCard";
-import Crechesmieuxnotees from "../Components/Crechesmieuxnotees";
+import Services from "../Components/service/Services.js";
+import Faq from "../Components/Faq/Faq.js";
+import deco3 from "../assets/deco3.svg";
+import Avis from "../Components/Aviss/Avis";
+import AvisCard from "../Components/Aviss/AvisCard";
+import FaqCard from "../Components/Faq/FaqCard";
+import CrecheCard from "../Components/CrechesMN/CrecheCard";
+import Crechesmieuxnotees from "../Components/CrechesMN/Crechesmieuxnotees";
 function Home() {
   return (
     <>
       <Header />
       <Hero />
-
       <div>
         <img src={deco1} />
       </div>
       <About />
       <Crechesmieuxnotees />
-      {/*  <CrecheCard/> */}
-      {/* <Promo /> */}
       <Services />
-
-      {/* <div className="flex justify-end">
-<img className=" md:h-[300px] " src={deco3} alt="deco3" />
-</div> */}
-      {/*   <Avis />  */}
-      {/*   <AvisCard /> */}
+      <Avis />
       <Faq />
-
       <Footer />
     </>
   );
