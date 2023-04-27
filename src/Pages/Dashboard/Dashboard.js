83d101dac34ee825abe95ec7c35fd32aae403b8f
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-04-27 22:12:50 +0100

    add dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,10 @@
 import NavAfter from "../../Components/NavAfter";
 import Notification from "../../Components/Notification";
 import { Stat } from "./Stat";
-
+import Notif from "./Notif";
+import mehdi from "../../assets/mehdi.jpeg"
+import linepurple from "../../assets/linepurple.svg"
+import lineyellow from "../../assets/lineyellow.svg"
 export const Dashboard = ({nom}) => {
 return(
     <>
@@ -14,23 +17,19 @@ return(
 
 <div className="font-Medium text-xl text-rawdablack font-Poppins ml-[100px] mt-[30px] ">    
 <h1>
-    Bonjour {nom}
+    Bonjour, {nom}
 </h1>
 </div> 
 
 <div className="flex gap-4">
 
-<div className="flex flex-col gap-4">
-<Stat/>
-
-
-
-
-
+<div className="flex flex-col gap-1">
+<Stat line={linepurple} titre="Creches ajoutees" number="2500" pourcentage="4.5"/>
+<Stat line={lineyellow} titre="Enfants inscris" number="6000" pourcentage="8"/>
 </div>
 
 
-
+{/* <Notif type="Demande de rendez-vous" photo={mehdi} nomc="Mehdi MAMOUNI" heuree="2" heure="16:30" date="27/05/2023" num="0560000000" email="km_mamouni@esi.dz"/> */}
 
 
 
--- src/Pages/Dashboard/Stat.js
+++ src/Pages/Dashboard/Stat.js
@@ -1,12 +1,41 @@
-export const Stat = ({line,titre,number,pourcentage})=> {
-return(
 
-<div className="bg-rawdawhite ml-[100px] mt-[30px] h-[120px] w-[300px] mb-4 rounded-xl">
+import arrow from "../../assets/arroww.svg";
+export const Stat = ({ line, titre, number, pourcentage }) => {
+  return (
+    <div className="bg-rawdawhite ml-[100px] mt-[30px] h-[150px] w-[360px]  rounded-xl">
+      <div className="flex flex-col gap-4">
+        <div className="flex ml-8 mt-6 gap-8">
+          <img src={line} />
+          <div className="flex flex-col gap-2">
+            <h1 className="text-rawdablack text-xl font-medium">
+              {titre}
+            </h1>
+            <h2 className="text-rawdablack text-3xl font-bold">{number}</h2>
+          </div>
+        </div>
+<div className="flex ml-10 gap-4">
+<img src={arrow}/>
+
+<div className="flex gap-1">
+<h3 className="text-md  text-[#159B5B] ">
+  +{pourcentage}%  
+</h3>
+<h3 className="text-md text-black text-opacity-40">
+depuis la semaine passée
+</h3>
+
 
 </div>
 
-);
 
 
 
-}
+</div>
+
+
+
+
+      </div>
+    </div>
+  );
+};
