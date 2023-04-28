751b0dd593a87d9e258e5628884a8d0140c833ba
rxgofast <129177416+rxgofast@users.noreply.github.com>
2023-04-28 20:58:42 +0100

    Update Login.js

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,99 +1,105 @@
-import React, { useState } from 'react';
-import image1 from '../assets/iluss2.png';
-import logo from '../assets/rawda.svg';
+import React, { useState, Component } from 'react';
+import axios from 'axios';
+import { useNavigate , Link } from 'react-router-dom';
+import image1 from '../assets/image1.svg';
+import image2 from '../assets/image2.svg';
+import image3 from '../assets/image3.svg';
+import logo from '../assets/logo.svg';
 
-const Login = (props) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+const Login =(Props) => {
+  
+      const [email, setEmail] = useState('');
+      const [password, setPassword] = useState('');
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-   
-  };
+      const history=useNavigate();
+     
+      const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+      }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // handle form submission
-  };
+      const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+      }
 
-  return (
-    <div className="min-h-screen bg-rawdawhite">
-      <div className="block h-12  bg-white">
-        <img className="w-1/12  mt-4 ml-11" src={logo} alt="logo" />
-      </div>
+      const handleSubmit = async (event) => { /* envoie des infos vers le bakc */
+        event.preventDefault();
 
-      <div className="container mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
-        <div className="ml-5">
-          <div><h1 className="text-4xl font-body font-bold mt-6 text-rawdapurple">Bienvenue dans notre</h1></div>
-          <div><h1 className="text-4xl font-body font-bold text-rawdapurple">communauté</h1></div>
-          <div className="container mx-auto mt-10 grid grid-cols-1 lg:grid-cols-2 gap-1">
-          <p className="text-lg font-body text-gray-500 mb-8">Une nouvelle façon de prendre soin de votre famille commence ici</p>
-          <img className="w-full sm:w-[350px]" src={image1} alt="Image 1" />
-          </div>
-
-        </div>
-
-        <div className="px-8">
-          <div className="flex justify-center my-8">
-            <button className="px-8 py-3 bg-violet-800 hover:bg-violet-900 text-white font-body rounded-xl opacity-100 shadow-purple-600 focus:outline-none">
-              Se connecter avec Google
-            </button>
-          </div>
-
-          <p className="text-center font-body mb-8">Ou inscrivez-vous avec votre email</p>
-
-          <form onSubmit={handleSubmit}>
-            <label className="flex justify-center mb-4">
-              <input
-                className="p-3 font-body w-7/12  rounded-md bg-neutral-200 border-neutral-200 focus:outline-none"
-                type="email"
-                placeholder="Entrez votre adresse mail"
-                value={email}
-                onChange={handleEmailChange}
-              />
-            </label>
-
-            <label className="flex justify-center mb-8">
-              <input
-                className="p-3 w-7/12 font-body rounded-md bg-neutral-200 border-neutral-200 focus:outline-none"
-                type="password"
-                placeholder="Mot de passe"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </label>
-
-            <div className="flex justify-center my-8">
-              <button
-                className="px-8 py-3 font-body bg-violet-800 hover:bg-violet-900 text-white rounded-xl shadow-md opacity-100 shadow-purple-600 focus:outline-none"
-                type="submit"
-              >
-                Se connecter
-              </button>
-            </div>
-          </form>
-         
-          <div class="flex justify-center">
-              <section class=" border-b border-gray-500 border-opacity-50 border-solid w-80"></section>
-            </div>
-         
-          <div className="flex justify-center items-center my-8">
-            <span className="text-gray-500 font-body text-sm mr-2">Vous n'avez pas de compte ?</span>
-            <button
-              className="text-violet-700 font-body text-sm font-bold cursor-pointer"
-              onClick={() => props.onFormSwitch('Signup')}
-            >
-              S'inscrire
-            </button>
-          </div>
-        </div>
-        </div>
-        </div>
+        try {
+               await axios.post("http://localhost:8000/login" , {
+                        email,password
+                })
+                .then( res=>{
+                        if (res.data= "User Existed")
+                        { history("/", {state:{id:email}})}
+                })
         
+
+       }
+        catch(e){ console.log("USER NOT LOGGED")};
+        console.log('Email:', email);
+        console.log('Password:', password);
+      } /* fin de l'envoie */
+
+    return(
+        <div class="min-h-screen bg-purple-50">
+
+                     <div class=" h-10 bg-white">
+                          <img class="w-1/12 mt-4 ml-11" src={logo} alt="logo" />
+                     </div>
+
+                     <footer class="fixed bottom-0 w-full text-center">
+                          <p class="mb-4 text-sm text-purple-400">Copyright © 2023 Rawda.com.Tous droits réservés</p>
+                     </footer>
+
+                    <div class='flex flex-wrap'>
+                
+                          <div className=" w-5/12 mt-16 ml-11 relative ">
+                                <img src={image1} alt="Image 1" className="w-full" />
+                                <div className="ml-20 absolute top-0 left-0 w-full">
+                                        <img src={image2} alt="Image 2" className="w-full" />
+                                </div>
+
+                                <div className=" ml-11 absolute top-0 left-0 w-full">
+                                        <img src={image3} alt="Image 3" className=" w-10/12 " />
+                                </div>
+                        
+                          </div>
+
+
+                            <div class="relative ml-60 mt-5 w-3/12">
+
+                                    <div class=" flex justify-center items-center my-auto mx-auto ">
+                                            <button class="mb-4 mt-5 border-2 w-8/12 h-12 focus:outline-none bg-violet-800 hover:bg-violet-900 text-white rounded-xl opacity-100 shadow-purple-600" >  Se connecter avec google </button>
+                                    </div>
+
+                                    <p class=" mb-4 text-center">Ou inscrivez-vous avec votre email</p>
+
+                                    <form onSubmit={handleSubmit} >
+                            
+                                        <label>
+                                                <input class=" p-3 w-full mb-4 rounded-md bg-neutral-200 border-neutral-200 focus:outline-none" type="email" placeholder="Entrez votre adresse mail" value={email} onChange={handleEmailChange} />
+                                        </label>
+
+                                        <label>
+                                            <input class=" p-3 w-full rounded-md bg-neutral-200 border-neutral-200 focus:outline-none" type="password" placeholder="Mot de passe" value={password} onChange={handlePasswordChange} />
+                                        </label>
+
+                                        <div class="flex justify-center items-center my-auto mx-auto">
+                                                <button class="mb-7 mt-11 w-full h-12 border-2 focus:outline-none bg-violet-800 hover:bg-violet-900 text-white rounded-xl shadow-2xl opacity-100 shadow-purple-600" type="submit"> Se connecter </button>
+                                        </div>
+
+                                        <div class="mx-auto my-auto">
+                                                <section class=" border-b border-gray-500 border-opacity-50 border-solid w-80"></section>
+                                        </div>
+
+                                        <div class="flex justify-center items-center my-auto mx-auto mt-3">
+                                                <span className="mr-4 text-xs text-gray-500 "> Vous n'avez pas de compte ?</span> <buttom onClick={()=>Props.onFormSwitch('/Singup')} className="text-violet-700 text-xs font-bold cursor-pointer">S'inscrire</buttom>
+                                        </div>
+                                    </form>
+                            </div>
+                   </div>
+        </div>
     );
     
 }
