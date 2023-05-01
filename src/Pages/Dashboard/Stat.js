1d9cf527ee3d69359fbb767ae426044ddec3e44a
Mehdi Mamouni <77162470+thenotoriousmehdi@users.noreply.github.com>
2023-05-02 00:04:50 +0100

    update

--- src/Pages/Dashboard/Stat.js
+++ src/Pages/Dashboard/Stat.js
@@ -1,4 +1,3 @@
-
 import arrow from "../../assets/arroww.svg";
 export const Stat = ({ line, titre, number, pourcentage }) => {
   return (
@@ -7,34 +6,20 @@ export const Stat = ({ line, titre, number, pourcentage }) => {
         <div className="flex ml-8 mt-6 gap-8">
           <img src={line} />
           <div className="flex flex-col gap-2">
-            <h1 className="text-rawdablack text-xl font-medium">
-              {titre}
-            </h1>
+            <h1 className="text-rawdablack text-xl font-medium">{titre}</h1>
             <h2 className="text-rawdablack text-3xl font-bold">{number}</h2>
           </div>
         </div>
-<div className="flex ml-10 gap-4">
-<img src={arrow}/>
+        <div className="flex ml-10 gap-4">
+          <img src={arrow} />
 
-<div className="flex gap-1">
-<h3 className="text-md  text-[#159B5B] ">
-  +{pourcentage}%  
-</h3>
-<h3 className="text-md text-black text-opacity-40">
-depuis la semaine passée
-</h3>
-
-
-</div>
-
-
-
-
-</div>
-
-
-
-
+          <div className="flex gap-1">
+            <h3 className="text-md  text-[#159B5B] ">+{pourcentage}%</h3>
+            <h3 className="text-md text-black text-opacity-40">
+              depuis la semaine passée
+            </h3>
+          </div>
+        </div>
       </div>
     </div>
   );
