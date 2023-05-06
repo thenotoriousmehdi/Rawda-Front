d69846b9f169cb1f50859a0d3f738b1606ee20ae
mythsSIMOU <lw_beldjoudi@esi.dz>
2023-05-06 14:41:24 +0100

    test

--- src/Pages/CrechInfoCard/avis.js
+++ src/Pages/CrechInfoCard/avis.js
@@ -1,49 +1,53 @@
-
 import { profilsdata } from "./profilsdata";
-import { Swiper , SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import FormDialog from "./evaluationDialog";
-export default function Avis (){
-  
 
-return(
+import mehdi1 from "../../assets/wassim.PNG";
 
-      <div className="mx-16  my-16 bg-rawdapurple bg-opacity-5 p-5 font-sans  text-center rounded-md ">
-         
-         <div className="flex flex-col  gap md:gap-y-0 mb-10 items-center">
-          <h3  className=" font-body font-bold text-5xl leading-6  text-rawdapurple  mt-6 text-center commenth3 z-10"> Avis </h3>
-          <div className="   pt-4  w-24 h-4 bg-yellow-300 shadow-yellow-300 "></div></div>
-          <div className="w-full" >
-
-            
-           <Swiper  slidesPerView={3} 
-                    spaceBetween={30}
-                    cssMode={true}
-                    navigation
-                    pagination={ {clickable:true}}
-                    mousewheel={true}
-                    keyboard={true}
-                    modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                className=" p-8 "  >
-             { 
-             profilsdata.map(( profilcomment  ) =>(
-              <SwiperSlide   >
-                 <div className=" border  border-rawdapurple flex relative flex-col  h-4/6 rounded-lg items-center justify-center p-7 gap-4   " >
-                   <img src={profilcomment.image }  className="   w-12 top-0      h-12 " />
-                   <span className=" font-body font-semibold text-lg lg:text-xl leading-7 lg:leading-9   "
-                   
-                     >{profilcomment.name} </span>
-                   <p className="font-body font-medium text-base lg:text-lg text-black leading-7  text-left pl-3 " >{ profilcomment.comment}</p>
-                   
-                </div>
-              </SwiperSlide>) )  }
-           </Swiper> 
-                                  
-           <FormDialog/>
-           </div>
+export default function Avis({ creche }) {
+  const eva = creche.avis.evaluations;
+  return (
+    <div className="mx-16  my-16 bg-rawdapurple bg-opacity-5 p-5 font-sans  text-center rounded-md ">
+      <div className="flex flex-col  gap md:gap-y-0 mb-10 items-center">
+        <h3 className=" font-body font-bold text-5xl leading-6  text-rawdapurple  mt-6 text-center commenth3 z-10">
+          {" "}
+          Avis{" "}
+        </h3>
+        <div className="   pt-4  w-24 h-4 bg-yellow-300 shadow-yellow-300 "></div>
       </div>
-)
+      <div className="w-full">
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          cssMode={true}
+          navigation
+          pagination={{ clickable: true }}
+          mousewheel={true}
+          keyboard={true}
+          modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+          className=" p-8 "
+        >
+          {eva.map((profilcomment) => (
+            <SwiperSlide>
+              <div className=" border  border-rawdapurple flex relative flex-col  h-4/6 rounded-lg items-center justify-center p-7 gap-4   ">
+                <img src={mehdi1} className="w-12 h-12 rounded-full" />
+                <span className=" font-body font-semibold text-lg lg:text-xl leading-7 lg:leading-9   ">
+                  {profilcomment.nom}{" "}
+                </span>
+                <p className="font-body font-medium text-base lg:text-lg text-black leading-7  text-left pl-3 ">
+                  {profilcomment.commentaires}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <FormDialog />
+      </div>
+    </div>
+  );
 }
--- src/Pages/CrechInfoCard/evaluationDialog.js
+++ src/Pages/CrechInfoCard/evaluationDialog.js
@@ -1,14 +1,15 @@
-import * as React  from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import { TextareaAutosize } from '@mui/material';
-import { useRef } from 'react'
-import Rating from '@mui/material/Rating';
-import { useState } from 'react';
-
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import { TextareaAutosize } from "@mui/material";
+import { useRef } from "react";
+import Rating from "@mui/material/Rating";
+import { useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router";
 
 export default function FormDialog() {
   const [open, setOpen] = React.useState(false);
@@ -16,73 +17,123 @@ export default function FormDialog() {
   const handleClickOpen = () => {
     setOpen(true);
   };
- const [publie, setPublie] = useState(false);
+
+  const [publie, setPublie] = useState(false);
   const handleClose = () => {
-    setPublie(false);  
+    setPublie(false);
     setOpen(false);
   };
-   
 
   const textAreaRef = useRef(null);
 
-
   const handleLogValue = () => {
     console.log(textAreaRef.current.value);
-    console.log(value) ; 
-    setPublie(true);  
-   };
-
- 
-
-
+    console.log(value);
+    setPublie(true);
+    handleSubmit();
+  };
 
   const [value, setValue] = React.useState(0);
+
+  const { id } = useParams();
+  const handleSubmit = async () => {
+    try {
+      console.log(value + 15);
+      await axios.patch(`http://localhost:8000/evaluerCreche/${id}`, {
+        note: value,
+        commentaires: textAreaRef.current.value,
+      });
+    } catch (e) {
+      console.log("Une erreur s'est produite");
+    }
+  };
+
   return (
     <div>
-<Button variant="outlined" onClick={handleClickOpen}  sx={ {mt:6 ,width:160 , height:50,  border:'solid #6938EF 2px' ,fontFamily:'Poppins',  color: 'black',
- fontSize: 20 , fontWeight: 400 ,  '&:hover': {border:'#6643b5 solid 1px'}  }} >
+      <Button
+        variant="outlined"
+        onClick={handleClickOpen}
+        sx={{
+          mt: 6,
+          width: 160,
+          height: 50,
+          border: "solid #6938EF 2px",
+          fontFamily: "Poppins",
+          color: "black",
+          fontSize: 20,
+          fontWeight: 400,
+          "&:hover": { border: "#6643b5 solid 1px" },
+        }}
+      >
         EVALUER
       </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle sx={{fontFamily:'Poppins' ,fontWeight: 600 , 
-      backgroundImage: 'linear-gradient(100deg, #f3f30f 100%, #fefefe 0%)',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: '40% .4em',
-      backgroundPosition: '6% 64%',
-      
-      }}>Evaluation de la créche </DialogTitle>
+        <DialogTitle
+          sx={{
+            fontFamily: "Poppins",
+            fontWeight: 600,
+            backgroundImage:
+              "linear-gradient(100deg, #f3f30f 100%, #fefefe 0%)",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "40% .4em",
+            backgroundPosition: "6% 64%",
+          }}
+        >
+          Evaluation de la créche{" "}
+        </DialogTitle>
         <DialogContent>
-   
-      
-      <Rating
-        name="simple-controlled"
-        size="large"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-     
-   
+          <Rating
+            name="simple-controlled"
+            size="large"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          />
 
-            <TextareaAutosize
-              aria-label="minimum height"
-              minRows={7}
-              ref={textAreaRef}
-             placeholder="Laisser un commentaire (moins de 150 mots)"
-             style={{ width: 500 }}
-             sx={{mt:6 ,  border:'#6938EF solid 1px' , '&:focus': {border:'#6938EF solid 1px'} }}
-             className=' bg-rawdapurple bg-opacity-5  p-3 font-body  '
+          <TextareaAutosize
+            aria-label="minimum height"
+            minRows={7}
+            ref={textAreaRef}
+            placeholder="Laisser un commentaire (moins de 150 mots)"
+            style={{ width: 500 }}
+            sx={{
+              mt: 6,
+              border: "#6938EF solid 1px",
+              "&:focus": { border: "#6938EF solid 1px" },
+            }}
+            className=" bg-rawdapurple bg-opacity-5  p-3 font-body  "
           />
         </DialogContent>
         <DialogActions>
-        
-        { !publie ?  <Button onClick={handleClose} sx={{ color:'#6938EF' , mb:2,fontFamily:'Poppins' , '&:hover': {border:'#6643b5 solid 1px'} }}>Annuler</Button>
-: null}
-         <Button  onClick={handleLogValue} sx={{ color:'white', mr:2 , mb:2,fontFamily:'Poppins'  ,'&:hover': {color:'#6938EF' , border:'#6643b5 solid 1px'} ,  background:'#6938EF' }}> { !publie ?   'Publier' : 'Publié!'}</Button>
+          {!publie ? (
+            <Button
+              onClick={handleClose}
+              sx={{
+                color: "#6938EF",
+                mb: 2,
+                fontFamily: "Poppins",
+                "&:hover": { border: "#6643b5 solid 1px" },
+              }}
+            >
+              Annuler
+            </Button>
+          ) : null}
+          <Button
+            onClick={handleLogValue}
+            sx={{
+              color: "white",
+              mr: 2,
+              mb: 2,
+              fontFamily: "Poppins",
+              "&:hover": { color: "#6938EF", border: "#6643b5 solid 1px" },
+              background: "#6938EF",
+            }}
+          >
+            {!publie ? "Publier" : "Publié!"}
+          </Button>
         </DialogActions>
       </Dialog>
     </div>
   );
 }
- 
