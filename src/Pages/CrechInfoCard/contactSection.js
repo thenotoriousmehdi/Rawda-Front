b61c34d01138a36d12b5907ba8105029f8de2678
Kahinasd <127248108+Kahinasd@users.noreply.github.com>
2023-05-07 20:54:40 +0100

    Update contactSection.js

--- src/Pages/CrechInfoCard/contactSection.js
+++ src/Pages/CrechInfoCard/contactSection.js
@@ -1,6 +1,26 @@
 import CustomizedDialogs from "./inscriptionDialog";
 import CustomizedDialogsRdv from "./rendezvousDialog";
 
+import fileDownload from 'js-file-download'
+ 
+
+
+/*import axios from 'axios'
+import fileDownload from 'js-file-download'
+ .
+
+handleDownload = (url, filename) => {
+  axios.get(url, {
+    responseType: 'blob',
+  })
+  .then((res) => {
+    fileDownload(res.data, filename)
+  })
+}
+
+<button onClick={() => {this.handleDownload('https://your-website.com/your-image.jpg', 'test-download.jpg')
+}}>Download Image</button>
+}*/
 
 export default function ContactSection({ creche }) {
 
